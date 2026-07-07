7ae6352165fa810d66d124d329ef40489d88a1e8
Camila Sol Hernandez Greco <chernandezgreco@frba.utn.edu.ar>
2026-07-07 00:07:58 -0300

    start.spring.io iniciado

---
+++ src/main/java/ar/utn/ba/dssi/ClimalertCami/ClimalertCamiApplication.java
@@ -0,0 +1,13 @@
+package ar.utn.ba.dssi.ClimalertCami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimalertCamiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimalertCamiApplication.class, args);
+	}
+
+}
